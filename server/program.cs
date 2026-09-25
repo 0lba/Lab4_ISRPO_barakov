0516fc3c065a6571a0be58a00b1e8054d0ed4a50
0lba <ebarakova0@gmail.com>
2026-09-25 18:24:24 +0300

    part 3

---
+++ server/program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.InteropServices;
+bool e = false;
+while(!e){
+    Console.WriteLine("Выберите что надо сделать:");
+    Console.WriteLine("1. ФИО");
+    Console.WriteLine("2. Группа");
+    Console.WriteLine("3. Текущая дата и время");
+    Console.WriteLine("4. Выход");
+    string c = Console.ReadLine();
+    switch (c)
+    {
+        case "1":
+            Console.WriteLine("Бараков Олег Юрьевич");
+            break;
+        case "2":
+            Console.WriteLine("ИСП-241");
+            break;
+        case "3":
+            Console.WriteLine("25.09.2026, 18:14");
+            break;
+        case "4":
+            e = true;
+            break;
+    }
+}
+Console.WriteLine("Нажмите любую кнопку");
+Console.ReadKey();
